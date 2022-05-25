7415e7c46265f52a616eb6bc62f207a59919370d
gary <gary.white@lodgit.com>
2022-05-25 13:26:42 +0200

    code working

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function array(arr) {
-    //Good luck
-
+    if (arr.split(',').slice(1, -1).join(' ') == '') {
+        return null;
+    } else {
+        return arr.split(',').slice(1, -1).join(' ');
+    }
 }
 
 console.log(array('1,2,3')); // '2'
-console.log(array('1,2,3,4')); // '2,3'
-console.log(array('1,2,3,4,5')); // '2,3,4,'
+console.log(array('1,2,3,4')); // '2 3'
+console.log(array('1,2,3,4,5')); // '2 3 4,'
 console.log(array('')); // null
