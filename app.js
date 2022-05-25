09d3b93fbffaf162a3ce34e74cd3bbee99cde558
gary <gary.white@lodgit.com>
2022-05-25 13:05:32 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function array(arr) {
+    //Good luck
+
+}
+
+console.log(array('1,2,3')); // '2'
+console.log(array('1,2,3,4')); // '2,3'
+console.log(array('1,2,3,4,5')); // '2,3,4,'
+console.log(array('')); // null
